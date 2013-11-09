9b1763dc0db585ad10512b1a4d16ca742293a269
Nicolas Guillemot <nlguillemot@gmail.com>
2013-11-09 15:31:53 -0800

    Added stub GitSnippetSource

---
+++ src/radorbad/GitSnippetSource.java
@@ -0,0 +1,11 @@
+package radorbad;
+
+public class GitSnippetSource implements IVCSSnippetSource {
+
+	@Override
+	public String GetSnippet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
