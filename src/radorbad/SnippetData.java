24d6378116b5e37c0fe5f8306519bfa710346d57
Nicolas Guillemot <nlguillemot@gmail.com>
2013-11-09 21:03:03 -0800

    snippets now return an ID also

---
+++ src/radorbad/SnippetData.java
@@ -0,0 +1,12 @@
+package radorbad;
+
+public class SnippetData {
+	
+	public SnippetData(String contents, long hash) {
+		this.contents = contents;
+		this.hash = hash;
+	}
+	
+	public String contents;
+	public long hash;
+}
