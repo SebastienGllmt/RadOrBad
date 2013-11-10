6bea9df0d06cf89c199436201675a3eeeb2727ff
Sebastien <SebastienGllmt@gmail.com>
2013-11-09 16:28:40 -0800

    Added Event Listeners

--- src/radorbad/ViewModel.java
+++ src/radorbad/ViewModel.java
@@ -1,16 +1,34 @@
 package radorbad;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JTextField;
 
 public class ViewModel {
 	
 	public static final ArrayList<JComponent> components = new ArrayList<JComponent>(){
 		{
-			this.add(new JButton("Rad"));
-			this.add(new JButton("Bad"));
+			JTextField code = new JTextField("Bob");
+			this.add(code);
+			JButton radButton = new JButton("Rad");
+			radButton.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e){
+					System.out.println("RAD");
+				}
+			});
+			this.add(radButton);
+			
+			JButton badButton = new JButton("Bad");
+			badButton.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e){
+					System.out.println("BAD");
+				}
+			});
+			this.add(badButton);
 		}
 	};
 }
